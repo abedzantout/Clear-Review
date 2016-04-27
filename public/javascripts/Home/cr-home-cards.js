ec6009d5c8ecfda6a719184cc47dca5aa82cd99a
AbedZantout <abed-zantout@live.com>
2016-04-27 23:32:39 +0300

    fixed search - updated delete button

--- public/javascripts/Home/cr-home-cards.js
+++ public/javascripts/Home/cr-home-cards.js
@@ -2,13 +2,16 @@ window.onload = function(){
     //SHOW DIALOG FOR CourseProfileDetails
     var dialogCourseProfile = document.getElementById("CourseProfile");
     var CourseProfilebutton = document.getElementsByClassName("CourseCard");
-
-
     for(var i = 0; i<CourseProfilebutton.length; i++){
         if (! dialogCourseProfile.showModal) {
             dialogPolyfill.registerDialog(dialogCourseProfile);
         }
         CourseProfilebutton[i].addEventListener('click', function() {
+            var CCardName = this.parentElement.parentElement.firstElementChild.firstElementChild;
+            var CCardDescription = this.parentElement.parentElement.children[1];
+            var CourseName = document.getElementsByClassName("CourseName")[0].innerHTML = CCardName.innerHTML;
+            var CourseDescription  = document.getElementsByClassName("CourseTitle")[0].innerHTML = CCardDescription.innerHTML;
+            // var CoursesTitle = document.getElementsByClassName("CourseTitle")[0].innerHTML = "The courses below are being taught by" + ProfName.innerHTML;
             dialogCourseProfile.showModal();
         });
         dialogCourseProfile.querySelector('.close').addEventListener('click', function() {
@@ -24,10 +27,24 @@ window.onload = function(){
             dialogPolyfill.registerDialog(dialogprofessorProfile);
         }
         ProfessorProfilebutton[i].addEventListener('click', function() {
+            var ProfName = this.parentElement.parentElement.firstElementChild.firstElementChild;
+            var ProfessorTitle = document.getElementsByClassName("ProfessorTitle")[0].innerHTML = ProfName.innerHTML;
+            var ProfessorCoursesTitle = document.getElementsByClassName("ProfessorCoursesTitle")[0].innerHTML = "The courses below are being taught by" + ProfName.innerHTML;
+
+
+
+
             dialogprofessorProfile.showModal();
+
+
         });
         dialogprofessorProfile.querySelector('.close').addEventListener('click', function() {
             dialogprofessorProfile.close();
         });
     }
+
+
+
+
+
 };
--- routes/home.js
+++ routes/home.js
@@ -25,7 +25,6 @@ module.exports = function (app, connection, io) {
 
     });
 
-
     connection.query('SELECT FIRST_NAME, SURNAME from Teacher', function (err, rows, fields) {
         if (err) throw err;
         var FIRST_NAME = [];
@@ -50,16 +49,17 @@ module.exports = function (app, connection, io) {
 
     });
 
-
-    connection.query('SELECT NUMBER, TITLE, SUBJECT FROM COURSE', function (err, rows, fields) {
+    connection.query('SELECT NUMBER, TITLE, SUBJECT, CREDIT_NUM FROM COURSE', function (err, rows, fields) {
         if (err) throw err;
         var NUMBER = [];
         var TITLE = [];
         var SUBJECT = [];
+        var CREDIT_NUM = []
         for (var i = 0; i < rows.length; i++) {
             NUMBER.push(rows[i].NUMBER);
             TITLE.push(rows[i].TITLE);
             SUBJECT.push(rows[i].SUBJECT);
+            CREDIT_NUM.push(rows[i].CREDIT_NUM)
         }
         console.log(NUMBER, TITLE, SUBJECT);
 
@@ -67,11 +67,10 @@ module.exports = function (app, connection, io) {
             socket.emit('courseDetails', {
                 NUMBER: NUMBER,
                 TITLE: TITLE,
-                SUBJECT: SUBJECT
+                SUBJECT: SUBJECT,
+                CREDIT_NUM: CREDIT_NUM
             });
         });
-
-
     });
 
     app.get('/home', function (req, res) {
