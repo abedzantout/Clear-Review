32d22e417989a55025c3f1eba9ab2e09d1519d9b
AbedZantout <abed-zantout@live.com>
2016-05-17 22:25:11 +0300

    milestone 3

--- routes/home.js
+++ routes/home.js
@@ -24,17 +24,7 @@ module.exports = function (app, connection, io, passport) {
             });
         });
     });
-    // connection.query('INSERT INTO ENROLL VALUES(?,?)', function(err, rows, fields) {
-    //    if(err) throw err;
-    //     var data = [];
-    //     console.log(data);
-    //     io('connection', function(socket){
-    //         socket.emit('QandA', {
-    //             //TODO: to modify
-    //             data: data
-    //         })
-    //     })
-    // });
+
     connection.query('SELECT FIRST_NAME, SURNAME from Teacher', function (err, rows, fields) {
         if (err) throw err;
         var FIRST_NAME = [];
@@ -60,8 +50,16 @@ module.exports = function (app, connection, io, passport) {
     io.on('connection', function (socket) {
         socket.on('checkcrn', function (msg) {
             console.log("message " + msg);
-            checkCRN(connection, msg, socket)
+            checkCRN(connection, msg, socket);
         });
+        socket.on('deleteCourse', function(msg) {
+            var subject = msg.substr(0, msg.indexOf(' '));
+            var number  = msg.substr(msg.indexOf(' ') + 1);
+            deleteCourse(subject, number);
+            console.log("delete course: " + subject + " " + number);
+        })
+
+
     });
 
     function checkCRN(connection, CRN, socket) {
@@ -73,34 +71,72 @@ module.exports = function (app, connection, io, passport) {
                 if (err) throw err;
                 if (rows.length < 1) {
                     //this is where i want to emit the message
-                    socket.emit('checkcrn', "error")
+                    socket.emit('checkcrn', "error");
                     //emitMessage('Course does not exist')
                 }
                 else if (rows.length > 0) {
                     socket.emit('checkcrn', rows[0].CRN);
-                    checkStudentIfEnrolloed(connection, rows[0].CRN);
+                    checkStudentIfEnrolloed(connection, rows[0].CRN,socket);
                 }
             });
     }
 
-    function checkStudentIfEnrolloed(connection, CRN) {
-        var query = "SELECT CRN FROM ENROLL WHERE CRN = ? AND STUDENTID = 2"
+
+
+    loadCourse();
+    function loadCourse() {
+        var loadCourses = "SELECT TITLE, NUMBER, SUBJECT, CREDIT_NUM FROM ENROLL e JOIN COURSEINSTANCE ci ON (e.CRN = ci.CRN) JOIN COURSE c ON (c.courseid = ci.COURSEID) WHERE e.STUDENTID = 2"
+        connection.query({
+            sql: loadCourses
+        }, function (err, rows, fields) {
+            if (err) throw err;
+            var TITLE = [], NUMBER = [], SUBJECT = [], CREDIT_NUM = [];
+            for (var i = 0; i < rows.length; i++) {
+                TITLE.push(rows[i].TITLE);
+                SUBJECT.push(rows[i].SUBJECT);
+                NUMBER.push(rows[i].NUMBER);
+                CREDIT_NUM.push(rows[i].CREDIT_NUM);
+            }
+            io.on('connection', function (socket) {
+                socket.emit('loadcourses', {
+                    NUMBER: NUMBER,
+                    TITLE: TITLE,
+                    SUBJECT: SUBJECT,
+                    CREDIT_NUM: CREDIT_NUM
+                });
+            });
+        })
+    }
+
+    function deleteCourse(SUBJECT, NUMBER){
+        var query = "DELETE e FROM ENROLL e JOIN COURSEINSTANCE ci ON (e.CRN = ci.CRN) join COURSE c ON(ci.COURSEID = c.COURSEID) WHERE c.SUBJECT = ? AND c.NUMBER = ? AND e.STUDENTID = 2";
+        connection.query({
+            sql: query,
+            values: [SUBJECT, NUMBER]
+        }, function(err, rows, fields){
+            if(err) throw err;
+            console.log(rows);
+        })
+    }
+
+
+
+    function checkStudentIfEnrolloed(connection, CRN,socket) {
+        var query = "SELECT CRN FROM ENROLL WHERE CRN = ? AND STUDENTID = 2";
         connection.query({
             sql: query,
             values: CRN
         }, function (err, rows, fields) {
             if (err) throw err;
             if (rows.length < 1) {
-                addCourse(connection, CRN);
+                addCourse(connection, CRN,socket);
             }
             else {
-                console.log(rows[0].CRN)
-                console.log('YOU CANT ADD A COURSE')
+                socket.emit('checkcrn',"error");
             }
         })
     }
-
-    function addCourse(connection, CRN) {
+    function addCourse(connection, CRN, socket) {
         var query = "INSERT INTO ENROLL VALUES(?, 2)"
         connection.query({
             sql: query,
@@ -108,10 +144,9 @@ module.exports = function (app, connection, io, passport) {
         }, function (err, rows, fields) {
             if (err) throw err;
             console.log(rows);
+            socket.emit('checkcrn', CRN);
         });
     }
-
-
     function add(connection, FIRST_NAME, SURNAME, io) {
         var queryCoursesTaughtBy = "SELECT COURSE.COURSEID,COURSE.TITLE,COURSE.NUMBER,COURSE.CREDIT_NUM FROM COURSE,COURSEINSTANCE,TEACHER WHERE TEACHER.FIRST_NAME = ? AND TEACHER.SURNAME = ? AND TEACHER.TEACHERID = COURSEINSTANCE.TEACHERID AND COURSEINSTANCE.COURSEID = COURSE.COURSEID"
         connection.query({
@@ -141,7 +176,6 @@ module.exports = function (app, connection, io, passport) {
             });
         });
     }
-
 
     connection.query('SELECT NUMBER, TITLE, SUBJECT, CREDIT_NUM FROM COURSE', function (err, rows, fields) {
         if (err) throw err;
